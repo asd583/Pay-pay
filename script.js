69d6e0ea8aad300daf48ee2cd4873224e03a0e73
asd583 <77786444+asd583@users.noreply.github.com>
2025-09-11 03:26:29 +0630

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,1 @@
-// Select product
-function selectItem(itemName, price){
-  document.getElementById('itemSelected').value = itemName;
-  document.getElementById('amount').value = price;
 
-  // Smooth scroll to payment section
-  document.getElementById('paymentForm').scrollIntoView({ behavior: 'smooth' });
-}
